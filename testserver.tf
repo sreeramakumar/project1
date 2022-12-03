f1268b6449790f0ad4f69e5f004860ce4e74ce1e
sreeramakumar <113412432+sreeramakumar@users.noreply.github.com>
2022-12-03 13:39:23 +0530

    updated testserver resouces of pemkey and etc

--- testserver.tf
+++ testserver.tf
@@ -6,6 +6,9 @@ provider "aws" {
 resource "aws_instance" "test_server" {
   ami           = "ami-074dc0a6f6c764218"
   instance_type = "t2.micro"
+  key_name = "sri-key"
+   subnet_id = "subnet-0f840cec82fced399"
+   security_groups = ["sg-0e80014777a0f2a6a"]
   tags = {
     Environment = "testserver"
     Name = "test_server"
