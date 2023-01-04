5c7131e299a9ffc7502d7bb8b0ce14365687e81e
sreeramakumar <113412432+sreeramakumar@users.noreply.github.com>
2023-01-04 17:46:28 +0530

    Update testserver.tf

--- testserver.tf
+++ testserver.tf
@@ -1,14 +1,14 @@
 provider "aws" {
   region     = "ap-south-1"
 }
-resource "aws_instance" "production_server" {
+resource "aws_instance" "test_server" {
   ami           = "ami-074dc0a6f6c764218"
   instance_type = "t2.micro"
   key_name = "sri-key"
    subnet_id = "subnet-0f840cec82fced399"
    security_groups = ["sg-0e80014777a0f2a6a"]
   tags = {
-    Env = "production"
-    Name = "production_server"
+    Env = "testserver"
+    Name = "test_server"
   }
 }
